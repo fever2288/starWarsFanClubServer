d7c1ed361f2335732c69ac6e1e022a8ec577743b
Srdjan <srdjan2288@hotmail.com>
2020-07-14 00:37:49 +0200

    Swagger API documentation

--- server.js
+++ server.js
@@ -3,26 +3,32 @@ const dotEnv = require('dotenv');
 const cors = require('cors');
 dotEnv.config();
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-})
+});
 
 app.use('/api/v1/starWars', require('./routes/starWarsRoutes'));
 
-//error handler middlware
-app.use(function (err, req, res, next) {
-    console.log(err.stack);
-    res.status(500).send({
-      status: 500,
-      message: err.message,
-      body: {},
-    })
-  })
+// API Documentation
+if (process.env.NODE_ENV != 'production') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
-
+// error handler middlware
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send({
+    status: 500,
+    message: err.message,
+    body: {},
+  });
+});
